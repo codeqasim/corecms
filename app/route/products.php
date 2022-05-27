805bb9c22b0b6d46ceac52c315be6ebad8fdfbd0
Qasim Hussain <compoxition@gmail.com>
2022-05-26 23:55:37 -0700

    Update products.php

--- app/route/products.php
+++ app/route/products.php
@@ -236,6 +236,11 @@ $router->post('account/products/update', function() {
     `product_features` = '".$_REQUEST['product_features']."',
     `product_brand_id` = '".$_REQUEST['product_brand_id']."',
 
+    `product_approval` = '0',
+    `product_status` = '".$product_status."',
+    `product_stock_id` = '0',
+    `product_city_id` = '',
+
     `product_name` = '".$_REQUEST['product_name']."'
     WHERE `products`.`product_id` = ".$_REQUEST['product_id'].";";
 
