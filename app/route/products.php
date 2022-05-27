56afc81e5f29922a6ce61a9c228d51946523ecde
Qasim Hussain <compoxition@gmail.com>
2022-05-26 20:40:50 -0700

    Update products.php

--- app/route/products.php
+++ app/route/products.php
@@ -288,7 +288,7 @@ $router->post('account/products/upload', function() {
 
         $date = date("yy:m:d:h:i");
         // $sql = "INSERT INTO `products_images` (`image_id`, `image_name`,`image_product_id`, `image_created_at`) VALUES (NULL, '".$img."', '".$queries['source']."','".$date."');";
-        $sql = "INSERT INTO `products_images` (`image_id`, `image_product_id`, `image_created_at`, `image_updated_at`, `image_name`) VALUES (NULL, '2', '2022-05-27 03:37:34.000000', '2022-05-27 03:37:34.000000', 'name.jpg');";
+        $sql = "INSERT INTO `products_images` (`image_id`, `image_product_id`, `image_created_at`, `image_updated_at`, `image_name`) VALUES (NULL, '".$queries['source']."', ".$date."', '', '".$img."');";
 
         // MYSQL UPDATE QUERY
         $query = "UPDATE `products` SET `product_img` = '".$img."' WHERE `products`.`product_id` = '".$queries['source']."';";
